1ad66228c64262166723d03ed328e8d327a4890e
chanchalod <chanchal.rajput@overdose.digital>
2024-01-11 15:21:38 +0530

    style: add hover effect

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,10 +13,10 @@ const Buttons = ({ children, variant, size = 'small', onPress }: ButtonTypes) =>
   return (
     <Button onPress={onPress}
       className={classNames("px-4 py-2 border-none outline-none text-white rounded-md max-w-max flex-none", {
-        "bg-sky-600": variant === "primary",
-        "bg-gray-700": variant === "secondary",
-        "bg-green-600": variant === "success",
-        "!text-blue-900": variant === "link",
+        "bg-sky-600 hover:bg-orange-500": variant === "primary",
+        "bg-gray-700 hover:bg-gray-500": variant === "secondary",
+        "bg-green-600 hover:bg-green-900": variant === "success",
+        "!text-blue-900 hover:!text-amber-600": variant === "link",
         "text-sm h-10": size === "small",
         "text-lg h-12": size === "medium",
         "text-xl h-14": size === "large",
